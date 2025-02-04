c2cf19aa3dcb72a08f094f6475ab095386e94502
AdvaitBothe1105 <advaitbothe12@gmail.com>
2025-02-04 19:14:55 +0530

    search bar responsive

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,101 +1,136 @@
 import "./Navbar.css";
 import logo from "../../Assets/ASDesigns_Logo.png";
-
-
+import '../../App.css'
 
 import { FaSearch } from "react-icons/fa";
 import { FaRegHeart } from "react-icons/fa6";
 import { LuShoppingCart } from "react-icons/lu";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 import { useUser, useAuth, useSession } from "@clerk/clerk-react";
+import { Skeleton } from "./skeleton";
+// import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
 export const Navbar = () => {
   const { user } = useUser();
 
-	return (
-		<nav className="navbar navbar-expand-md ">
-			<div className="d-flex flex-column w-100">
-				<div className="container-fluid d-flex w-100">
-					<a className="navbar-brand" href="#">
-						<img
-							className='m-3'
-							src={logo}
-							alt="Bootstrap"
-							width={200}
-							height={50}
-						/>
-					</a>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarSupportedContent"
-						aria-controls="navbarSupportedContent"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-					>
-						<span className="navbar-toggler-icon" />
-					</button>
-					<div className="collapse navbar-collapse" id="navbarSupportedContent">
-						<form className="d-flex mx-auto">
-							<div className="input-group">
+  return (
+    <nav className="navbar navbar-expand-md">
+      <div className="d-flex flex-column w-100">
+        <div className="container-fluid d-flex w-100">
+          <a className="navbar-brand" href="#">
+            <img
+              className="m-3"
+              src={logo}
+              alt="Bootstrap"
+              width={200}
+              height={50}
+            />
+          </a>
+          {/* <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button> */}
 
-								<input
-									className="form-control border-0 bg-light"
-									style={{ width: '18rem', margin: '0 0 0 11rem' }}
-									type="search"
-									placeholder="Search"
-									aria-label="Search"
-								/>
-								<span className="input-group-text bg-light border-0">
-									<FaSearch className="text-muted" />
-								</span>
-							</div>
-						</form>
-					</div>
-					<div className="nav-icons">
-						<LuShoppingCart className='icon' />
-						Cart
-						
-					</div>
-					<div className="nav-icons">
-						<FaRegHeart className=' icon' />
-						Wishlist
-					</div>
-					{user ? (
-						<h1>user icon present</h1>
-					) : (
-						<h1>user icon absent</h1>
-					)}
-					<SignedOut>
-						<SignInButton />
-					</SignedOut>
-					<SignedIn>
-						<UserButton />
-					</SignedIn>
-				</div>
-				<div className="container-fluid w-100">
-					<ul class="nav nav-underline">
-						<li class="nav-item ps-3">
-							<a class="nav-link text-dark" aria-current="page" href="#">Home</a>
-						</li>
-						<li class="nav-item ps-3">
-							<a class="nav-link text-dark" href="#shop">Shop</a>
-						</li>
-						<li class="nav-item ps-3">
-							<a class="nav-link text-dark" href="#sports">Sports</a>
-						</li>
-						<li class="nav-item ps-3">
-							<a class="nav-link text-dark" href="#festival">Festival</a>
-						</li>
-						<li class="nav-item ps-3">
-							<a class="nav-link text-dark" href="#others">Others</a>
-						</li>
-					</ul>
-				</div>
-				{/* <hr/> */}
-			</div>
+          <form className="d-none d-md-flex mx-auto">
+            <div className="input-group mt-10">
+              <input
+                className="form-control border-0 bg-light"
+                style={{ width: "18rem", height: "2rem" }}
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button
+                className="input-group-text bg-light border-0"
+                style={{ height: "2rem" }}
+              >
+                <FaSearch className="text-muted" />
+              </button>
+            </div>
+          </form>
+          <div className="nav-icons">
+            <LuShoppingCart className="icon" />
+            Cart
+          </div>
+          <div className="nav-icons">
+            <FaRegHeart className="icon" />
+            Wishlist
+          </div>
+          <div className="nav-login-icon">
+            {user ? null : (
+              // <Avatar>
+              //   <AvatarImage src="https://github.com/shadcn.png" />
+              //   <AvatarFallback>CN</AvatarFallback>
+              // </Avatar>
+              <h1>User absent</h1>
+            )}
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </div>
 
-		</nav >
-	);
+        {/* {Mobile Search}       */}
+        <div className="container-fluid d-md-none mt-2 mb-3">
+          <form className="d-flex w-100">
+            <div className="input-group w-100">
+              <input
+                className="form-control border-0 bg-light"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <span className="input-group-text bg-light border-0">
+                <FaSearch className="text-muted" />
+              </span>
+            </div>
+          </form>
+        </div>
+        <div className="container-fluid w-100">
+          <ul className="nav nav-underline">
+            <li className="nav-item ps-3">
+              <a className="nav-link text-dark" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item ps-3">
+              <a className="nav-link text-dark" href="#shop">
+                Shop
+              </a>
+            </li>
+            <li className="nav-item ps-3">
+              <a className="nav-link text-dark" href="#sports">
+                Sports
+              </a>
+            </li>
+            <li className="nav-item ps-3">
+              <a className="nav-link text-dark" href="#festival">
+                Festival
+              </a>
+            </li>
+            <li className="nav-item ps-3">
+              <a className="nav-link text-dark" href="#others">
+                Others
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
---
+++ client/src/Components/Navbar/skeleton.jsx
@@ -0,0 +1,11 @@
+import { cn } from "@/lib/utils"
+import '../../App.css';
+
+function Skeleton({
+  className,
+  ...props
+}) {
+  return (<div className={cn("animate-pulse rounded-md bg-muted", className)} {...props} />);
+}
+
+export { Skeleton }
