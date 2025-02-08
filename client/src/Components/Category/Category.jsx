11bb384b1f5f617773ea4ddb130c0e1fa5fab6f9
Sahilpatil135 <sahilrp2005@gmail.com>
2025-02-08 15:59:51 +0530

    Minor Changes

--- client/src/Components/Category/Category.jsx
+++ client/src/Components/Category/Category.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import "./Category.css"
 
 export const Category = () => {
   return (
-    <div>
+    <div className="category">
       <div className="text-center">
         <h1 className="category-head">Our Category</h1>
       </div>
