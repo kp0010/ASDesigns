3295cd92fdd9aaa08a90344f5f13e3d6087c2f97
Sahilpatil135 <sahilrp2005@gmail.com>
2025-02-22 00:40:30 +0530

    Minor changes

--- client/src/Pages/Shop.jsx
+++ client/src/Pages/Shop.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
+import "./CSS/Shop.css"
+import "../App.css"
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import { BasicBreadcrumbs } from '@/Components/Breadcrumbs/Breadcrumbs';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
 
 export const Shop = () => {
+  const { productId } = useParams()
+
+  const [product, setProduct] = useState({})
+  const [categories, setCategories] = useState([])
+
   return (
-    <div>Shop</div>
+    <div>
+      <div className="shop-header">
+        <h2>Shop</h2>
+      </div>
+      <div className="shop-breadcrumb ml-9 mt-3">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">Home</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/shop">Shop</BreadcrumbLink>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+      
+      <div className="shop-sort">
+        <h2>sorting</h2>
+      </div>
+    </div>
   )
 }
