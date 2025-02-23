1a38e8adeddef605b73972c06f3e64a11d7aacb5
AdvaitBothe1105 <advaitbothe12@gmail.com>
2025-02-23 18:47:53 +0530

    Responsive Wishlist Done

--- client/src/Components/WishProducts/WishProducts.jsx
+++ client/src/Components/WishProducts/WishProducts.jsx
@@ -5,14 +5,14 @@ import { IoCartOutline } from "react-icons/io5";
 import { IoCloudDownloadOutline } from "react-icons/io5";
 import { FaRegTrashAlt } from "react-icons/fa";
 
-
 export const WishProducts = () => {
   return (
     <div className="wish bg-[#edeae7] flex flex-col items-center">
       <h2 className="text-4xl text-center pt-10 mb-5">Your Wishlists</h2>
-      <div className="cards bg-white pt-2 w-[80%] rounded-lg flex  mb-5 ">
+
+      <div className="cards bg-white pt-2 w-[80%] rounded-lg flex flex-col md:flex-row items-center md:items-start mb-5 p-4">
         <div className="product-display-left mt-4">
-          <div className="productDiplay-img h-[200px] w-[200px] ml-8">
+          <div className="productDiplay-img h-[200px] w-[200px] md:ml-8">
             <img
               src={prod_img}
               className="product-display-main-img rounded-lg"
@@ -20,31 +20,25 @@ export const WishProducts = () => {
             />
           </div>
         </div>
-        <div className="product-display-right ml-28">
-          <h2 className="text-2xl mt-4">
-            JD001 | Ultra Lightweight Festival Jersey
-            {/* {productId + (product["name"] ? " | " + product["name"] : "")} */}
-          </h2>
 
-          <div className="price flex mt-3 items-center">
-            <h1 className="new-price font-bold text-2xl mr-4">
-              ₹499.90
-              {/* ₹{(parseFloat(product.price) - 1.0).toFixed(2)} */}
-            </h1>
-            <h1 className="old-price text-xl line-through">
-              ₹699.90
-              {/* ₹{(parseFloat(product.price) + 200.0).toFixed(2)} */}
-            </h1>
-            <h3 className="text-xl ml-3">(-40% off)</h3>
+        <div className="product-display-right md:ml-20 ">
+            <h2 className="text-2xl mt-4 break-words ml-9 md:ml-0 md:text-left lg:text-left" > JD001 | Ultralight Festival Jersey</h2>
+
+
+          <div className="price flex flex-col md:flex-row mt-3 items-center">
+            <h1 className="new-price font-bold text-2xl md:mr-4">₹499.90</h1>
+            <h1 className="old-price text-xl line-through">₹699.90</h1>
+            <h3 className="text-xl md:ml-3">(-40% off)</h3>
           </div>
-          <div className="buy-section mt-5">
-            <Button className="mr-3 w-72 md:mb-3 bg-black">
+
+          <div className="buy-section mt-5 flex flex-col md:flex-col lg:flex-row xl:flex-row md:flex-wrap md:justify-start gap-2 w-full">
+            <Button className="w-full md:w-60 text-sm py-2 bg-black flex items-center justify-center mr-3">
               <IoCartOutline /> Add to Cart
             </Button>
-            <Button className="w-72 mr-3 md:mb-3 bg-[#e3c756]">
+            <Button className="w-full md:w-60 xl:w-70 lg:w-50 text-sm py-2 bg-[#e3c756] flex items-center justify-center mr-3">
               <IoCloudDownloadOutline /> Download
             </Button>
-            <Button className="w-40 mr-3 md:mb-3 bg-white text-black border-2 border-black">
+            <Button className="w-full md:w-32 text-sm py-2 bg-white text-black border-2 border-black flex items-center justify-center mr-3">
               <FaRegTrashAlt /> Remove
             </Button>
           </div>
