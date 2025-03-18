865452d9f2edd70ba5e8ac45c3313beb1214949a
Sahilpatil135 <sahilrp2005@gmail.com>
2025-03-18 21:56:57 +0530

    Collapsible Sidebar done

--- client/src/Components/Admin/AdminSidebar/AdminSidebar.jsx
+++ client/src/Components/Admin/AdminSidebar/AdminSidebar.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
-import { Home, LucideLayoutDashboard } from "lucide-react"
-import { FaProductHunt } from "react-icons/fa";
+import React, { useState } from "react";
+import { Home, LucideLayoutDashboard, LuChevronDown, LuChevronRight, LuUsers, LuList } from "lucide-react";
+import { MdProductionQuantityLimits } from "react-icons/md";
+import { BsTags } from "react-icons/bs";
+import { BiCategoryAlt } from "react-icons/bi";
+import { IoAddOutline } from "react-icons/io5";
+
+
 import logo from "../../../Assets/Logos/AS_logo_b.png";
 import { Link } from "react-router-dom";
 
@@ -18,46 +23,22 @@ import {
   SidebarMenuSubButton,
   SidebarMenuSubItem,
 } from "@/Components/ui/sidebar"
-
-// const items = [
-//   { title: "Home", url: "/", icon: <Home /> },
-//   { title: "Dashboard", url: "/admin", icon: <Home/> },   // need to add exact dashboard route
-//   { title: "Inbox", url: "#", icon: <Inbox /> },
-//   { title: "Calendar", url: "#", icon: <Calendar /> },
-//   { title: "Search", url: "#", icon: <Search /> },
-//   { title: "Settings", url: "#", icon: <Settings /> },
-// ];
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/Components/ui/collapsible';
 
 export const AdminSidebar = () => {
+  const [isProductOpen, setIsProductOpen] = useState(true);
+  const [isCategoryOpen, setIsCategoryOpen] = useState(true);
+  const [isTagsOpen, setIsTagsOpen] = useState(true);
   return (
     <Sidebar>
-      {/* <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <Link to={item.url}>
-                      {item.icon}
-                      <span>{item.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent> */}
       <SidebarHeader>
         <SidebarMenu className="my-3">
           <SidebarMenuItem>
             <SidebarMenuButton>
               <Link to="/" className="flex">
                 <img className="m-3" src={logo} alt="..." width={50} height={50} />        {/*add border bottom*/}
+                <span className="text-xl font-bold">ASDesigns</span>
               </Link>
-              <span className="text-xl font-bold">ASDesigns</span>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
@@ -65,6 +46,7 @@ export const AdminSidebar = () => {
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupContent>
+
             {/* ✅Dashboard */}
             <SidebarMenu className="mb-2">
               <SidebarMenuItem>
@@ -76,122 +58,148 @@ export const AdminSidebar = () => {
                 </SidebarMenuButton>
               </SidebarMenuItem>
             </SidebarMenu>
-            {/* add collapsible here  */}
+
             {/* ✅Products */}
             <SidebarMenu className="mb-2">
-              <SidebarMenuItem>
-                <SidebarMenuButton>
-                  <div className="flex m-3" >
-                    <FaProductHunt />
-                    <span className="ml-3 text-base" >Products</span>
-                  </div>
-                </SidebarMenuButton>
-                <SidebarMenuSub className="mt-2">
-                  <SidebarMenuSubItem>
-                    <SidebarMenuSubButton>
-                      <Link to="products/add" className="flex m-2">
-                        <Home />
-                        <span className="ml-3 text-base">Add Product</span>
-                      </Link>
-                    </SidebarMenuSubButton>
-                  </SidebarMenuSubItem>
-                </SidebarMenuSub>
-                <SidebarMenuSub className="mt-2">
-                  <SidebarMenuSubItem>
-                    <SidebarMenuSubButton >        {/*can add asChild ahead of sidebarmenusubbutton. asChild used reducing the size of subbutton*/}
-                      <Link to="" className="flex m-2">
-                        <Home />
-                        <span className="ml-3 text-base">Delete Product</span>
-                      </Link>
-                    </SidebarMenuSubButton>
-                  </SidebarMenuSubItem>
-                </SidebarMenuSub>
-                <SidebarMenuSub className="mt-2">
-                  <SidebarMenuSubItem>
-                    <SidebarMenuSubButton>
-                      <Link to="" className="flex m-2">
-                        <Home />
-                        <span className="ml-3 text-base">Edit Product</span>
-                      </Link>
-                    </SidebarMenuSubButton>
-                  </SidebarMenuSubItem>
-                </SidebarMenuSub>
-                <SidebarMenuSub className="mt-2">
-                  <SidebarMenuSubItem>
-                    <SidebarMenuSubButton>
-                      <Link to="" className="flex m-2">
-                        <Home />
-                        <span className="ml-3 text-base">Product List</span>
-                      </Link>
-                    </SidebarMenuSubButton>
-                  </SidebarMenuSubItem>
-                </SidebarMenuSub>
-              </SidebarMenuItem>
+              <Collapsible defaultOpen className="group/collapsible" open={isProductOpen} onOpenChange={setIsProductOpen} >
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>
+                      <div className="flex m-3 items-center" >
+                        <MdProductionQuantityLimits className="w-6 h-6" />
+                        <span className="ml-3 text-base" >Products</span>
+                        {isProductOpen ? <LuChevronDown className="ml-20" /> : <LuChevronRight className="ml-20" />}
+                      </div>
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub className="mt-2">
+                      <SidebarMenuSubItem>
+                        <SidebarMenuSubButton>
+                          <Link to="products/add" className="flex m-2">
+                            <IoAddOutline className="w-6 h-6" />
+                            <span className="ml-3 text-base">Add Product</span>
+                          </Link>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    </SidebarMenuSub>
+                    <SidebarMenuSub className="mt-2">
+                      <SidebarMenuSubItem>
+                        <SidebarMenuSubButton >
+                          <Link to="" className="flex m-2">
+                            <Home />
+                            <span className="ml-3 text-base">Delete Product</span>
+                          </Link>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    </SidebarMenuSub>
+                    <SidebarMenuSub className="mt-2">
+                      <SidebarMenuSubItem>
+                        <SidebarMenuSubButton>
+                          <Link to="" className="flex m-2">
+                            <Home />
+                            <span className="ml-3 text-base">Edit Product</span>
+                          </Link>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    </SidebarMenuSub>
+                    <SidebarMenuSub className="mt-2">
+                      <SidebarMenuSubItem>
+                        <SidebarMenuSubButton>
+                          <Link to="" className="flex m-2">
+                            <LuList className="w-6 h-6" />
+                            <span className="ml-3 text-base">Product List</span>
+                          </Link>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+
+                </SidebarMenuItem>
+              </Collapsible>
             </SidebarMenu>
-            {/* add collapsible  */}
+
             {/* ✅Category  */}
             <SidebarMenu className="mb-2">
-              <SidebarMenuItem>
-                <SidebarMenuButton>
-                  <Link to="" className="flex m-3">
-                    <Home />
-                    <span className="ml-3 text-base">Category</span>
-                  </Link>
-                </SidebarMenuButton>
-                <SidebarMenuSub className="mt-2">
-                  <SidebarMenuSubItem>
-                    <SidebarMenuSubButton>
-                      <Link to="" className="flex m-2">
-                        <Home />
-                        <span className="ml-3 text-base">Add Category</span>
-                      </Link>
-                    </SidebarMenuSubButton>
-                  </SidebarMenuSubItem>
-                </SidebarMenuSub>
-                <SidebarMenuSub className="mt-2">
-                  <SidebarMenuSubItem>
-                    <SidebarMenuSubButton>
-                      <Link to="" className="flex m-2">
-                        <Home />
-                        <span className="ml-3 text-base">Category List</span>
-                      </Link>
-                    </SidebarMenuSubButton>
-                  </SidebarMenuSubItem>
-                </SidebarMenuSub>
-                {/* add category edit/delete  */}
-              </SidebarMenuItem>
+              <Collapsible defaultOpen className="group/collapsible" open={isCategoryOpen} onOpenChange={setIsCategoryOpen} >
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>
+                      <div className="flex m-3 items-center">
+                        <BiCategoryAlt className="w-6 h-6" />
+                        <span className="ml-3 text-base">Category</span>
+                        {isCategoryOpen ? <LuChevronDown className="ml-20" /> : <LuChevronRight className="ml-20" />}
+                      </div>
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub className="mt-2">
+                      <SidebarMenuSubItem>
+                        <SidebarMenuSubButton>
+                          <Link to="" className="flex m-2">
+                            <IoAddOutline className="w-6 h-6" />
+                            <span className="ml-3 text-base">Add Category</span>
+                          </Link>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    </SidebarMenuSub>
+                    <SidebarMenuSub className="mt-2">
+                      <SidebarMenuSubItem>
+                        <SidebarMenuSubButton>
+                          <Link to="" className="flex m-2">
+                            <LuList className="w-6 h-6" />
+                            <span className="ml-3 text-base">Category List</span>
+                          </Link>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                  {/* add category edit/delete  */}
+                </SidebarMenuItem>
+              </Collapsible>
+
             </SidebarMenu>
             {/* add collapsible  */}
             {/* ✅Tags  */}
             <SidebarMenu className="mb-2">
-              <SidebarMenuItem>
-                <SidebarMenuButton>
-                  <Link to="" className="flex m-3">
-                    <Home />
-                    <span className="ml-3 text-base">Tags</span>
-                  </Link>
-                </SidebarMenuButton>
-                <SidebarMenuSub className="mt-2">
-                  <SidebarMenuSubItem>
-                    <SidebarMenuSubButton>
-                      <Link to="" className="flex m-2">
-                        <Home />
-                        <span className="ml-3 text-base">Add Tags</span>
-                      </Link>
-                    </SidebarMenuSubButton>
-                  </SidebarMenuSubItem>
-                </SidebarMenuSub>
-                <SidebarMenuSub className="mt-2">
-                  <SidebarMenuSubItem>
-                    <SidebarMenuSubButton>
-                      <Link to="" className="flex m-2">
-                        <Home />
-                        <span className="ml-3 text-base">Tags List</span>
-                      </Link>
-                    </SidebarMenuSubButton>
-                  </SidebarMenuSubItem>
-                </SidebarMenuSub>
-              </SidebarMenuItem>
+              <Collapsible defaultOpen className="group/collapsible" open={isTagsOpen} onOpenChange={setIsTagsOpen} >
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>
+                      <div className="flex m-3 items-center justify-evenly">      {/*Advait*/}
+                        <BsTags className="w-6 h-6" />
+                        <div className="flex items-center justify-evenly">        {/*Advait*/}
+                          <span className="ml-3 text-base">Tags</span>
+                          {isTagsOpen ? <LuChevronDown className="" /> : <LuChevronRight className="" />}
+                        </div>
+                      </div>
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub className="mt-2">
+                      <SidebarMenuSubItem>
+                        <SidebarMenuSubButton>
+                          <Link to="" className="flex m-2">
+                            <IoAddOutline className="w-6 h-6" />
+                            <span className="ml-3 text-base">Add Tags</span>
+                          </Link>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    </SidebarMenuSub>
+                    <SidebarMenuSub className="mt-2">
+                      <SidebarMenuSubItem>
+                        <SidebarMenuSubButton>
+                          <Link to="" className="flex m-2">
+                            <LuList className="w-6 h-6" />
+                            <span className="ml-3 text-base">Tags List</span>
+                          </Link>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+
             </SidebarMenu>
             {/* ✅Previous Orders  */}
             <SidebarMenu className="mb-2">
@@ -209,7 +217,7 @@ export const AdminSidebar = () => {
               <SidebarMenuItem>
                 <SidebarMenuButton>
                   <Link to="" className="flex m-3">
-                    <Home />
+                    <LuUsers className="w-6 h-6" />
                     <span className="ml-3 text-base">Users</span>
                   </Link>
                 </SidebarMenuButton>
